8b86e7c2adaa78724a010f6e5cfd424533425630
Aaditya Sriram <aaditya@aadityasriram.com>
2014-05-17 08:45:10 +0000

    NewQuestionServlet.java edited online with Bitbucket, added comments to explain the servlet

--- src/com/footbalfrenzy/quizapp/NewQuestionServlet.java
+++ src/com/footbalfrenzy/quizapp/NewQuestionServlet.java
@@ -11,9 +11,15 @@ import com.footballfrenzy.quizapp.dao.DatastoreImpl;
 import com.footballfrenzy.quizapp.dataobjects.Question;
 import com.google.gson.Gson;
 
+/*
+	This is the servlet for handling new question creation request.
+	If you see the servlet has a method doPost which means it expects HTTP Post request
+	The post request must contain a JSON string for a question Object.
+*/
 @SuppressWarnings("serial")
 public class NewQuestionServlet extends HttpServlet {
 	
+	// Expects a HTTP Post request
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
@@ -24,7 +30,7 @@ public class NewQuestionServlet extends HttpServlet {
 		Datastore datastore = new DatastoreImpl();
 		datastore.addQuestion(question);
 		
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Question Added");
 	}
 
 }
